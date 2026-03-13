ba86b51bae6995d2bc13242703f75c49be6742ce
Timmy Patrick <70456825+timmypatrick@users.noreply.github.com>
2026-03-13 19:32:56 +0100

    Add files via upload

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("/nav_bar.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("navbar-container").innerHTML = data;
-    });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   fetch("/nav_bar.html")
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.getElementById("navbar-container").innerHTML = data;
+//     });
+// });
+
+fetch("/nav_bar/nav_bar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("nav_bar").innerHTML = data;
+  });
